93e7b1998b0a74dc327eb19753bc730f2d3e141a
Marcus Downing <marcus.downing@gmail.com>
2026-08-06 17:24:57 +0100

    Separate BG script file

--- src/www/js/script.js.h
+++ src/www/js/script.js.h
@@ -4,27 +4,8 @@
 {{> home_js }}
 {{> recommendations_js }}
 {{> history_js }}
+{{> bg_js }}
 
-// select a background image
-function selectBackground() {
-    var DAY_LIMIT = 5;
-    var NIGHT_LIMIT = 7;
-
-    var hour = new Date().getHours();
-    var isDay = hour >= 6 && hour < 18;
-    var bgNum = 1+Math.floor(Math.random() * (isDay ? DAY_LIMIT : NIGHT_LIMIT));
-    var bg = (isDay ? "day-" : "night-")+bgNum;
-
-    if (isDay) {
-      document.body.classList.add('day');
-      document.body.classList.remove('night');
-    } else {
-      document.body.classList.add('day');
-      document.body.classList.remove('night');
-    }
-
-    document.body.setAttribute('bg', bg);
-}
 
 function parseQuery(queryString) {
     var query = {};
@@ -87,7 +68,6 @@ window.onload = function () {
     console.log(e);
   }
 };
-setInterval(selectBackground, 300000); // 5 minutes
 
 function expandComingSoon() {
   let elem = document.getElementById('coming-soon');
